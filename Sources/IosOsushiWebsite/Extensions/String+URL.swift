5b0525f631f71787e11b14550f67ce07c861fdbe
uhooi <the_uhooi@yahoo.co.jp>
2022-04-06 09:52:30 +0900

    Rename removed to decoded

--- Sources/IosOsushiWebsite/Extensions/String+URL.swift
+++ Sources/IosOsushiWebsite/Extensions/String+URL.swift
@@ -3,7 +3,7 @@ import Foundation
 extension String {
     func urlEncoded() -> String {
         let allowedChars = CharacterSet.alphanumerics.union(.init(charactersIn: "/?-._~"))
-        let removed = removingPercentEncoding ?? self
-        return removed.addingPercentEncoding(withAllowedCharacters: allowedChars) ?? removed
+        let decoded = removingPercentEncoding ?? self
+        return decoded.addingPercentEncoding(withAllowedCharacters: allowedChars) ?? decoded
     }
 }
